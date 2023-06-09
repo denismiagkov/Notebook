ae91cee5590e043a7f6ac6b0eb1a2037625a13d5
Denis Myagkov <tomich-84@mail.ru>
2023-06-10 00:02:29 +0300

    second commit service & presenter

---
+++ Presenter/Presenter.py
@@ -0,0 +1,46 @@
+
+from typing import List
+from datetime import date
+from Note.Note import Note
+from Service.Service import Service
+from UI.View import View
+
+
+class Presenter:
+    def __init__(self, service: Service, view: View):
+        self.service = service
+        self.view = view
+        view.set_presenter(self)
+
+    def show_list(self) -> List[str]:
+        titles = []
+        for n in self.service.get_notebook():
+            titles.append(n.get_title())
+        return titles
+
+    def show_notes_by_date(self, local_date: date) -> List[Note]:
+        return self.service.show_notes_by_date(local_date)
+
+    def get_note(self, title: str) -> str:
+        return str(self.service.get_note(title))
+
+    def add_note(self, title: str, body: str):
+        self.service.add_note(title, body)
+
+    def remove_note(self, title: str):
+        self.service.remove_note(title)
+
+    def contains_of(self, title: str) -> bool:
+        return self.service.contains_of(title)
+
+    def set_body(self, title: str, body: str):
+        self.service.set_body(title, body)
+
+    def set_date(self, title: str):
+        self.service.set_date(title)
+
+    def save_data(self, title: str):
+        self.service.save_data(title)
+
+    def load_data(self, title: str):
+        self.service.load_data(title)
---
+++ Service/Service.py
@@ -0,0 +1,47 @@
+
+from typing import List
+from datetime import datetime
+
+from InOut.InOutCsv import InOutCsv
+from Note.Note import Note
+from Notebook import Notebook
+
+
+class Service:
+    def __init__(self, notebook: Notebook):
+        self.id = 0
+        self.date = None
+        self.notebook = notebook
+        self.copy = InOutCsv()
+
+    def add_note(self, title: str, body: str):
+        date = datetime.now()
+        self.notebook.add_record(Note(self.id, date, title.upper(), body))
+        self.id += 1
+
+    def remove_note(self, title: str):
+        self.notebook.remove_record(title)
+
+    def get_notebook(self) -> List[Note]:
+        return self.notebook.get_notebook()
+
+    def get_note(self, title: str) -> Note:
+        return self.notebook.get_note(title)
+
+    def contains_of(self, title: str) -> bool:
+        return self.notebook.contains_of(title)
+
+    def set_body(self, title: str, body: str):
+        self.notebook.get_note(title).set_body(body)
+
+    def set_date(self, title: str):
+        self.notebook.get_note(title).set_date(datetime.now())
+
+    def show_notes_by_date(self, datetime: datetime) -> List[Note]:
+        return self.notebook.show_notes_by_date(datetime)
+
+    def save_data(self, title: str):
+        self.copy.save_data(self.notebook, title)
+
+    def load_data(self, title: str):
+        self.copy.load_data(title)
